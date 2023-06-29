0b00fcae4e5db3755af31da342eb6126c58299bd
Suchan An <dobestan@gmail.com>
2023-06-29 12:09:09 +0900

    Feature: Implement etherToTokenInput function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -6,7 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 contract Exchange is ERC20 {
-    constructor() ERC20("Uniswap V1", "UNI-V1") {}
+    address public tokenAddress;
+
+    constructor(
+        address _tokenAddress
+    ) ERC20("Uniswap V1", "UNI-V1") {
+        tokenAddress = _tokenAddress;
+    }
 
     function addLiquidity(
     ) public payable returns (uint lpTokenAmount) {
@@ -20,6 +26,14 @@ contract Exchange is ERC20 {
     function etherToTokenInput(
         uint minTokens
     ) public payable returns (uint tokensBought) {
+        uint etherSold = msg.value;
+        tokensBought = etherSold * 997 / 1000;
+        require(tokensBought >= minTokens);
+
+        ERC20 token = ERC20(tokenAddress);
+        token.transfer(msg.sender, tokensBought);
+
+        return tokensBought;
     }
 
     function etherToTokenOutput(
