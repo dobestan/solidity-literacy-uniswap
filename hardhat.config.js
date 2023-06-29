45d681b4195e5a87408dbad8159407b226a18c1e
Suchan An <dobestan@gmail.com>
2023-06-29 19:26:28 +0900

    Config: Add Goerli testnet config including Infura endpoint

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.20",
+    solidity: "0.8.20",
+    networks: {
+        goerli: {
+            url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: [
+                `${process.env.PRIVATE_KEY}`,
+            ],
+        },
+    },
 };
