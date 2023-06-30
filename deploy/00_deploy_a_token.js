842bd08a2ec4589d361ee10701d382f4c1d3ceec
Suchan An <dobestan@gmail.com>
2023-06-30 16:00:05 +0900

    Feature: Add deploy scripts to deploy token, factory, and exchange together

--- deploy/00_deploy_a_token.js
+++ deploy/00_deploy_a_token.js
@@ -1,5 +1,17 @@
 module.exports = async ({ getNamedAccounts, deployments, ethers }) => {
-  const { deploy, execute } = deployments;
+    console.log(`
+============================================================
+                    *** Deploying ***
+
+   / /   (_) /____  _________ ________  __
+  / /   / / __/ _ \/ ___/ __ '/ ___/ / / /
+ / /___/ / /_/  __/ /  / /_/ / /__/ /_/ /
+/_____/_/\__/\___/_/   \__,_/\___/\__, /   ... is love ❤️
+                                 /____/
+============================================================
+`)
+
+  const { deploy, getOrNull, execute } = deployments;
   const { deployer } = await getNamedAccounts();
 
   const ERC20Mintable = await deploy("ERC20Mintable", {
@@ -8,15 +20,20 @@ module.exports = async ({ getNamedAccounts, deployments, ethers }) => {
     log: true,
   });
 
-  await execute(
-    "ERC20Mintable",
-    { from: deployer },
-    "mint",
-    deployer,
-    ethers.utils.parseEther("10000")
-  );
-
-  console.log("Minted 10000 tokens to deployer");
+  const existingToken = await getOrNull("ERC20Mintable");
+  if (!existingToken) {
+    await execute(
+      "ERC20Mintable",
+      { from: deployer },
+      "mint",
+      deployer,
+      ethers.utils.parseEther("10000")
+    );
+    console.log("Minted 10000 tokens to deployer");
+  } else {
+    console.log("ERC20Mintable already deployed, skipping minting");
+  }
 };
+
 
 module.exports.tags = ["ERC20Mintable"];
